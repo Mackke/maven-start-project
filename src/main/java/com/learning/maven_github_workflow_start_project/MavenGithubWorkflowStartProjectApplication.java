eb0411f640a162893975393e0f7f0ec9e3301b45
mackke <mackke90@gmail.com>
2024-06-15 21:06:44 +0200

    add base directory

---
+++ src/main/java/com/learning/maven_github_workflow_start_project/MavenGithubWorkflowStartProjectApplication.java
@@ -0,0 +1,13 @@
+package com.learning.maven_github_workflow_start_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenGithubWorkflowStartProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenGithubWorkflowStartProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/maven_github_workflow_start_project/MavenGithubWorkflowStartProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.maven_github_workflow_start_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenGithubWorkflowStartProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
